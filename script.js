8da324596a165db62b37fbff777dc044d6531052
Varun Seth <8343593+varun-seth@users.noreply.github.com>
2022-12-04 01:47:08 +0530

    separate character types and automatic generation on change

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function generateRandomString() {
     var length = document.getElementById('lengthSlider').value;
     var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters = '';
+    if (document.getElementById('capitals').checked) characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (document.getElementById('smallCase').checked) characters += 'abcdefghijklmnopqrstuvwxyz';
+    if (document.getElementById('numbers').checked) characters += '0123456789';
+
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -11,4 +15,14 @@ function generateRandomString() {
 
 document.getElementById('lengthSlider').oninput = function() {
     document.getElementById('sliderValue').textContent = this.value;
+    generateRandomString();
 }
+
+// Add event listeners to checkboxes
+var checkboxes = document.querySelectorAll("input[type=checkbox]");
+checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', generateRandomString);
+});
+
+// Generate initial string
+window.onload = generateRandomString;
